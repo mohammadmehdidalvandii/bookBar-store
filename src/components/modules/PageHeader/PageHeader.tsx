e245b5ddb32327e4fb47b22de7e60d09a8f50edd
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-01-25 14:54:04 +0330

    created section pageHeader

---
+++ src/components/modules/PageHeader/PageHeader.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './PageHeader.css';
+import { Link } from 'react-router-dom';
+
+const PageHeader:React.FC = ({title}:String)=> {
+  return (
+    <section className="pageHeader">
+        <div className="container">
+            <div className="pageHeader_wrapper">
+                <h4 className="pageHeader_title">{title}</h4>
+                <Link to='/' className='pageHeader_link'>خانه</Link>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default PageHeader
--- src/page/Store/Store.tsx
+++ src/page/Store/Store.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import PageHeader from '../../components/modules/PageHeader/PageHeader'
 
 function Store():React.ReactElement {
   return (
     <>
-  
+    <PageHeader title='فروشگاه'/>
     </>
   )
 }
