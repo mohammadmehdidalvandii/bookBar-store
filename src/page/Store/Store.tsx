0615e7be017efe9247433d29742709ff5f6eee4a
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-01-25 14:33:32 +0330

    fixed page-store

--- src/page/Store/Store.tsx
+++ src/page/Store/Store.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Navbar from '../../components/modules/Navbar/Navbar'
-import Footer from '../../components/modules/Footer/Footer'
 
 function Store():React.ReactElement {
   return (
